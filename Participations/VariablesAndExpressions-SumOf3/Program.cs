7a8912989d48e66ba5ddbf13907e4d409cd20cc9
Ackerman, Adam P <adamackrmn@gmail.com>
2022-02-08 16:41:56 -0600

    COmpleted the sum of 3 participation

--- Participations/VariablesAndExpressions-SumOf3/Program.cs
+++ Participations/VariablesAndExpressions-SumOf3/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Please input your first number >>");
+string answer1 = Console.ReadLine();
+double val1 = Convert.ToDouble(answer1);
+
+Console.WriteLine("Please input your second number >>");
+string answer2 = Console.ReadLine();
+double val2 = Convert.ToDouble(answer2);
+
+Console.WriteLine("Please input your third number >>");
+string answer3 = Console.ReadLine();
+double val3 = Convert.ToDouble(answer3);
+
+double sum = val1 + val1 + val3;
+
+
+Console.WriteLine($"{val1.ToString("N3")} + {val2.ToString("N3")} + {val3.ToString("N3")} = {sum.ToString("N3")}");
+
+const double MAGIC_NUMBER = 7.777;
+double newTotal = sum * MAGIC_NUMBER;
+
+Console.WriteLine($"{sum.ToString("N3")} * {MAGIC_NUMBER.ToString("N3")} = {newTotal.ToString("N3")}");
