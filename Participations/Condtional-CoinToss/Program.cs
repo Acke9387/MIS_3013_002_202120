81d17b61a018e6809215460f2d77c692c6bb5c92
Ackerman, Adam P <adamackrmn@gmail.com>
2022-02-10 17:10:51 -0600

    Completed coin toss participation

---
+++ Participations/Condtional-CoinToss/Program.cs
@@ -0,0 +1,52 @@
+﻿const string DEVELOPER_INFORMATION = "Professor Ackerman";
+const int HEADS = 1;
+const int TAILS = 2;
+
+Random rand = new Random();
+
+int randomNumber = rand.Next(1, 3);
+
+//  Added for testing purposes
+//Console.WriteLine(randomNumber);
+
+Console.WriteLine("Heads or Tails? >>");
+string usersChoice = Console.ReadLine();
+int usersGuess = -1;
+
+if (usersChoice == "Heads")
+{
+    usersGuess = HEADS;
+}
+else if (usersChoice == "Tails")
+{
+    usersGuess = TAILS;
+}
+else
+{
+    //usersGuess = -1;
+    Console.WriteLine("Invalid input.");
+    Environment.Exit(usersGuess)
+}
+
+if (randomNumber == HEADS)
+{
+    Console.WriteLine("The coin landed heads up");
+}
+else
+{
+    Console.WriteLine("The coin landed tails up");
+}
+
+if (usersGuess == randomNumber)
+{
+    Console.WriteLine("Congratulations, you guessed correctly! =D");
+}
+else
+{
+    Console.WriteLine("Sorry, better luck next time. =(");
+}
+
+
+
+Console.WriteLine($"Developed by {DEVELOPER_INFORMATION}");
+
