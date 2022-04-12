35e4699addd0d1b4e4c546745f7019546ee2fbb4
Ackerman, Adam P <adamackrmn@gmail.com>
2022-04-12 17:45:11 -0500

    Completed in class example over classes

---
+++ In_Class_Examples/MethodsWithParallelArraysBUTWithClasses/Program.cs
@@ -0,0 +1,42 @@
+﻿
+string[] names = { "John", "Jane", "Marry", "Herman", "Kylie" };
+List<Student> students = new List<Student>();
+
+for (int i = 0; i < names.Length; i++)
+{
+    Student x = new Student();
+    x.Name = names[i];
+    x.Id = i + 1;
+    x.Grades = CreateRandomSizedListWithRandomValues();
+
+    students.Add(x);
+}
+
+
+foreach (Student student in students)
+{
+    //student.StudentInformation();
+
+    Console.WriteLine(student);
+}
+
+
+
+Student s = new Student(); ;
+s.Id = 2;
+
+
+static List<double> CreateRandomSizedListWithRandomValues()
+{
+    List<double> randoGrades = new List<double>();
+    Random rand = new Random();
+    int numberOfGrades = rand.Next(3, 8);
+
+    for (int i = 0; i < numberOfGrades; i++)
+    {
+        double grade = rand.NextDouble();
+        randoGrades.Add(grade);
+    }
+
+    return randoGrades;
+}
---
+++ In_Class_Examples/MethodsWithParallelArraysBUTWithClasses/Student.cs
@@ -0,0 +1,53 @@
+﻿
+public class Student
+{
+    public string Name { get; set; }
+
+    public int Id { get; set; }
+
+    public List<double> Grades { get; set; }
+
+    public Student()
+    {
+        Name = String.Empty;
+        Id = 0;
+        Grades = new List<double>();
+    }
+
+    public override string ToString()
+    {
+        double sum = 0, avg = 0;
+
+        foreach (double grade in Grades)
+        {
+            sum += grade;
+        }
+        //sum = studGrades.Sum();
+        //avg = studGrades.Average();
+
+        avg = sum / Grades.Count;
+
+        return $"{Name} ({Id}) has {Grades.Count.ToString("N")} # of grades with an average of {avg.ToString("P")}!";
+
+    }
+
+    public void StudentInformation()
+    {
+        double sum = 0, avg = 0;
+
+        foreach (double grade in Grades)
+        {
+            sum += grade;
+        }
+        //sum = studGrades.Sum();
+        //avg = studGrades.Average();
+
+        avg = sum / Grades.Count;
+
+        Console.WriteLine($"{Name} ({Id}) has {Grades.Count.ToString("N")} # of grades with an average of {avg.ToString("P")}!");
+
+    }
+
+
+}
+
