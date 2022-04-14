61a24e2d7857e01f1aacd77b65ee49ac13181909
Ackerman, Adam P <adamackrmn@gmail.com>
2022-04-14 16:47:54 -0500

    Completed the rectangle part of the in class example

---
+++ In_Class_Examples/Classes-RectangleAndCircle/Program.cs
@@ -0,0 +1,9 @@
+﻿Rectangle r1 = new Rectangle();
+r1.Width = 7;
+r1.Length = 10;
+
+//Console.WriteLine($"Length: {r1.Length}\nWidth:{r1.Width}\nArea:{r1.CalculateArea()}\nPerimeter:{r1.CalculatePerimeter()}");
+Console.WriteLine(r1);
+Rectangle r2 = new Rectangle(10, 5);
+//Console.WriteLine($"Length: {r2.Length}\nWidth:{r2.Width}\nArea:{r2.CalculateArea()}\nPerimeter:{r2.CalculatePerimeter()}");
+Console.WriteLine(r2);
---
+++ In_Class_Examples/Classes-RectangleAndCircle/Rectangle.cs
@@ -0,0 +1,41 @@
+﻿
+internal class Rectangle
+{
+
+    public double Length { get; set; }
+
+    public double Width { get; set; }
+
+
+    /// <summary>
+    /// Default/Empty Constructor
+    /// </summary>
+    public Rectangle()
+    {
+        Length = 0;
+        Width = 0;
+    }
+
+    public Rectangle(double length, double width)
+    {
+        Length = length;
+        Width = width;
+    }
+
+    public double CalculateArea()
+    {
+        return Width * Length;
+    }
+
+    public double CalculatePerimeter()
+    {
+        return 2 * (Length + Width);
+    }
+
+    public override string ToString()
+    {
+        return $"Length: {Length}\nWidth:{Width}\nArea:{CalculateArea()}\nPerimeter:{CalculatePerimeter()}";
+    }
+
+}
+
